b57357b66ae5279a0888951da1f04ddf888d972a
Zilla <zilla@Zilla-2.local>
2022-05-26 18:16:30 -0500

    created a easier way to write the combobox, need to do the same with other input fields

--- components/Inputs/Combobox.js
+++ components/Inputs/Combobox.js
@@ -14,120 +14,30 @@ const testData = [
 ];
 
 export default function Combobox(props) {
-  const [options, setOptions] = useState([]);
   const [showOptions, setShowOptions] = useState(false);
 
-  // 1. Search doesn't filter or anything for now
-  //    but if you needed to filter, call api, whatever
-  //    this is where you do it...
-  // 2. You'd probably want to add loading state too
   const onSearch = async (values) => {
     console.log("Search: ", values.text);
-    await callApiForOptions(); // Testing Only...
     setShowOptions(true);
-  };
-  const callApiForOptions = async () => {
-    await new Promise((resolve) => resolve(setOptions(testData)));
   };
 
   const onSubmit = async (values) => {
     console.log("Selected Values", values.comboboxSelected);
   };
 
-  // Sort Alphabetically
-  options.sort((a, b) => a.localeCompare(b));
-
   return (
     <>
       <Form
         onSubmit={onSubmit}
-        initialValues={{ text: "" }}
         render={({ handleSubmit, form, submitting, pristine, values }) => (
           <form onSubmit={handleSubmit}>
-            <div className="combobox-input-container">
-              <div className="combobox-input">
-                <Field name="text">
-                  {({ input, meta }) => (
-                    <div className="input-field">
-                      <Label name="text" label="Combo Box" hasError={false} />
-
-                      <div className="combobox-dropdown-options">
-                        <input
-                          {...input}
-                          type="text"
-                          placeholder='Type and click search to "Fetch" values...'
-                          className="text-input"
-                          onFocus={(e) => {
-                            const { value } = e.target;
-                            if (value !== "") return setShowOptions(true);
-                          }}
-                        />
-
-                        {/* List dropdown */}
-                        {options.length > 0 && showOptions ? (
-                          <div className="list-options combobox-list">
-                            <ul>
-                              {options.map((item, i) => {
-                                return (
-                                  <li
-                                    className={
-                                      values.comboboxSelected &&
-                                      values.comboboxSelected.includes(item)
-                                        ? "option active"
-                                        : "option"
-                                    }
-                                    key={i}
-                                  >
-                                    <Field
-                                      name="comboboxSelected"
-                                      value={item}
-                                      type="checkbox"
-                                      multiple={true}
-                                    >
-                                      {({ input, meta }) => (
-                                        <div className="checkbox-field combobox">
-                                          <label className="field-label">
-                                            {item}
-                                            <input
-                                              {...input}
-                                              type="checkbox"
-                                              className="checkbox"
-                                            />
-                                          </label>
-                                        </div>
-                                      )}
-                                    </Field>
-                                  </li>
-                                );
-                              })}
-                            </ul>
-                          </div>
-                        ) : null}
-                      </div>
-                    </div>
-                  )}
-                </Field>
-              </div>
-
-              {/* List Dropdown - Background Listener */}
-              {showOptions ? (
-                <span
-                  className="select-external-listener"
-                  onClick={() => setShowOptions(false)}
-                />
-              ) : null}
-
-              <div className="combobox-btn">
-                <div className="submit-container">
-                  <span
-                    className="submit-btn"
-                    onClick={() => onSearch(values.text)}
-                  >
-                    Search
-                  </span>
-                </div>
-              </div>
-            </div>
+            <RenderCombobox
+              values={values}
+              options={testData}
+              showOptions={showOptions}
+              setShowOptions={setShowOptions}
+              onSearch={onSearch}
+            />
 
             <div className="combobox-submit">
               <button
@@ -145,5 +55,97 @@ export default function Combobox(props) {
         )}
       />
     </>
+  );
+}
+
+function RenderCombobox(props) {
+  const { values, options, onSearch, showOptions, setShowOptions } = props;
+
+  // Sort Alphabetically
+  options.sort((a, b) => a.localeCompare(b));
+
+  return (
+    <div className="combobox-input-container">
+      <div className="combobox-input">
+        <Field name="text">
+          {({ input, meta }) => (
+            <div className="input-field">
+              <Label name="text" label="Combo Box" hasError={false} />
+
+              <div className="combobox-dropdown-options">
+                <input
+                  {...input}
+                  type="text"
+                  placeholder='Type and click search to "Fetch" values...'
+                  className="text-input"
+                  onFocus={(e) => {
+                    const { value } = e.target;
+                    if (value !== "") return setShowOptions(true);
+                  }}
+                />
+
+                {/* List dropdown */}
+                {options.length > 0 && showOptions ? (
+                  <div className="list-options combobox-list">
+                    <ul>
+                      {options.map((item, i) => {
+                        return (
+                          <li
+                            className={
+                              values &&
+                              values.comboboxSelected &&
+                              values.comboboxSelected.includes(item)
+                                ? "option active"
+                                : "option"
+                            }
+                            key={i}
+                          >
+                            <Field
+                              name="comboboxSelected"
+                              value={item}
+                              type="checkbox"
+                              multiple={true}
+                            >
+                              {({ input, meta }) => (
+                                <div className="checkbox-field combobox">
+                                  <label className="field-label">
+                                    {item}
+                                    <input
+                                      {...input}
+                                      type="checkbox"
+                                      className="checkbox"
+                                    />
+                                  </label>
+                                </div>
+                              )}
+                            </Field>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                ) : null}
+              </div>
+            </div>
+          )}
+        </Field>
+      </div>
+
+      {/* List Dropdown - Background Listener */}
+      {showOptions ? (
+        <span
+          className="select-external-listener"
+          onClick={() => setShowOptions(false)}
+        />
+      ) : null}
+
+      <div className="combobox-btn">
+        <div className="submit-container">
+          <span className="submit-btn" onClick={() => onSearch(values.text)}>
+            Search
+          </span>
+        </div>
+      </div>
+    </div>
   );
 }
